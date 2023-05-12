ea5c5ff2d07dd1c01afa991e9962abbfe7409945
jwalus <walusjacob@gmail.com>
2023-05-12 16:26:05 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Simple-User-Dashboard</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
